c32038758125b4d1fecaf9353455a1eb91ae39db
Chris Guevara <chris@homdna.com>
2016-03-06 19:07:07 -0800

    Use new folder/module structure

---
+++ www/states/states.module.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+angular.module('starter.states', [
+	'starter.states.browse',
+	'starter.states.playlist',
+	'starter.states.playlists',
+	'starter.states.root',
+	'starter.states.search'
+]);
+
+})();
